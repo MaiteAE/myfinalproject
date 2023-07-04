5f1070e3a931f56f4581b849f04ed3b0b6643be7
MaiteAE <maitearbones@gmail.com>
2023-07-04 13:09:29 +0200

    reajustes de css

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 
-  document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
-    var nombre = document.getElementsByName("nombre")[0].value;
-    var correo = document.getElementsByName("mail")[0].value;
-    var mensaje = document.getElementsByName("mensaje")[0].value;
+// document.getElementById("contact-form").addEventListener("submit", function (event) {
+//   event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+//   var nombre = document.getElementsByName("nombre")[0].value;
+//   var correo = document.getElementsByName("mail")[0].value;
+//   var mensaje = document.getElementsByName("mensaje")[0].value;
 
-    if (nombre === "" || correo === "" || mensaje === "") {
-      alert("Por favor, complete todos los campos");
-      return;
-    }
-    // Aquí se realizará la validación y el envío del formulario
-  });
+//   if (nombre === "" || correo === "" || mensaje === "") {
+//     alert("Por favor, complete todos los campos");
+//     return;
+//   }
+//   // Aquí se realizará la validación y el envío del formulario
+// });
 
