d07c844bfd35965feb1825398fb9fad7f7187450
MaiteAE <maitearbones@gmail.com>
2023-07-04 20:21:14 +0200

    solucion de errores

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # myfinalproject
 Desarrollo de todo lo aprendido en el modulo 1 del curso de Desarrollo web del SOC en el entorno de cliente, realizado con HTML, CSS, Tailwind, JavaScript
+Muestra un web de portfolio de Desarrollador web.
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 
-// document.getElementById("contact-form").addEventListener("submit", function (event) {
-//   event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
-//   var nombre = document.getElementsByName("nombre")[0].value;
-//   var correo = document.getElementsByName("mail")[0].value;
-//   var mensaje = document.getElementsByName("mensaje")[0].value;
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+  event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+  var nombre = document.getElementsByName("nombre")[0].value;
+  var correo = document.getElementsByName("mail")[0].value;
+  var mensaje = document.getElementsByName("mensaje")[0].value;
 
-//   if (nombre === "" || correo === "" || mensaje === "") {
-//     alert("Por favor, complete todos los campos");
-//     return;
-//   }
-//   // Aquí se realizará la validación y el envío del formulario
-// });
+  if (nombre === "" || correo === "" || mensaje === "") {
+    alert("Por favor, complete todos los campos");
+    return;
+  }
+  // Aquí se realizará la validación y el envío del formulario
+});
 
+
+const limit_size_screen = window.matchMedia('screen and (max-width: 768px)');
+const bars = document.querySelector('.bars');
+const barsButton = document.querySelector('#bars-button');
+
+function validation(event) {
+  if (event.matches) {
+    barsButton.addEventListener('click', hideShow);
+  } else {
+    barsButton.removeEventListener('click', hideShow);
+  }
+}
+
+function hideShow() {
+  bars.classList.toggle('is-active');
+}
+
+limit_size_screen.addEventListener('change', validation);
+validation(limit_size_screen);
