b91292f4d2eb8fad150b4e1ad9500d67a3ddb5b9
MaiteAE <maitearbones@gmail.com>
2023-07-05 11:09:57 +0200

    terminado todo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
-
-document.getElementById("contact-form").addEventListener("submit", function (event) {
-  event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
-  var nombre = document.getElementsByName("nombre")[0].value;
-  var correo = document.getElementsByName("mail")[0].value;
-  var mensaje = document.getElementsByName("mensaje")[0].value;
-
-  if (nombre === "" || correo === "" || mensaje === "") {
-    alert("Por favor, complete todos los campos");
-    return;
-  }
-  // Aquí se realizará la validación y el envío del formulario
-});
-
-
+//este es el codigo del menu hamburger
 const limit_size_screen = window.matchMedia('screen and (max-width: 768px)');
 const bars = document.querySelector('.bars');
 const barsButton = document.querySelector('#bars-button');
@@ -31,3 +17,18 @@ function hideShow() {
 
 limit_size_screen.addEventListener('change', validation);
 validation(limit_size_screen);
+
+// formulario de contacto
+const form= document.getElementById("contact-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+  var nombre = document.getElementsByName("nombre")[0].value;
+  var correo = document.getElementsByName("mail")[0].value;
+  var mensaje = document.getElementsByName("mensaje")[0].value;
+
+  if (nombre === "" || correo === "" || mensaje === "") {
+    alert("Por favor, complete todos los campos");
+    return;
+  } 
+form.reset()
+  });
